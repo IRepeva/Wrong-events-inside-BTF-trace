c9bb6383714141137b947688cbe8903464590ca4
IRepeva <irepeva@gmail.com>
2019-11-13 11:32:00 +0300

    files updated

--- unittests.py
+++ unittests.py
@@ -26,13 +26,19 @@ class FakeWriter(object):
 class WrongEventsTesting(TestCase):
     def setUp(self):
         self.fake_writer = FakeWriter()
-        self.fake_reader_parallel_start = FakeReader([
+
+    def test_simultaneous_start(self):
+        fake_reader = FakeReader([
             ("42", "Core1", "0", "T", "T2", "391", "activate"),
             ("43", "Core1", "0", "T", "T2", "391", "start"),
             ("44", "Core1", "0", "T", "T1", "392", "activate"),
             ("45", "Core1", "0", "T", "T1", "392", "start"),
         ])
-        self.fake_reader_parallel_start_resume = FakeReader([
+        wrong_events_search(fake_reader, self.fake_writer)
+        self.assertEqual(self.fake_writer.result, [('45', 4, 'Parallel active tasks')])
+
+    def test_simultaneous_start_resume(self):
+        fake_reader = FakeReader([
             ("42", "Core1", "0", "T", "T2", "391", "activate"),
             ("43", "Core1", "0", "T", "T2", "391", "start"),
             ("44", "Core1", "0", "T", "T2", "391", "preempt"),
@@ -40,68 +46,62 @@ class WrongEventsTesting(TestCase):
             ("46", "Core1", "0", "T", "T1", "392", "activate"),
             ("47", "Core1", "0", "T", "T1", "392", "start"),
         ])
-        self.fake_reader_wrong_order1 = FakeReader([
+        wrong_events_search(fake_reader, self.fake_writer)
+        self.assertEqual(self.fake_writer.result, [('47', 6, 'Parallel active tasks')])
+
+    def test_wrong_order1(self):
+        fake_reader = FakeReader([
             ("42", "Core1", "0", "T", "T2", "391", "activate"),
             ("43", "Core1", "0", "T", "T2", "391", "start"),
             ("44", "Core1", "0", "T", "T2", "392", "activate")
         ])
-        self.fake_reader_wrong_order2 = FakeReader([
+        wrong_events_search(fake_reader, self.fake_writer)
+        self.assertEqual(self.fake_writer.result, [('44', 3, 'Wrong order of events')])
+
+    def test_wrong_order2(self):
+        fake_reader = FakeReader([
             ("42", "Core1", "0", "T", "T2", "391", "activate"),
             ("43", "Core1", "0", "T", "T2", "391", "start"),
             ("44", "Core1", "0", "T", "T2", "391", "preempt"),
             ("45", "Core1", "0", "T", "T2", "392", "terminate")
         ])
-        self.fake_reader_first_event_start = FakeReader([
+        wrong_events_search(fake_reader, self.fake_writer)
+        self.assertEqual(self.fake_writer.result, [('45', 4, 'Wrong order of events')])
+
+    def test_first_start(self):
+        fake_reader = FakeReader([
             ("43", "Core1", "0", "T", "T2", "391", "start")
         ])
-        self.fake_reader_first_event_preempt = FakeReader([
+        wrong_events_search(fake_reader, self.fake_writer)
+        self.assertEqual(self.fake_writer.result, [('43', 1, 'Wrong order of events. First action is not "activate"')])
+
+    def test_first_preempt(self):
+        fake_reader = FakeReader([
             ("43", "Core1", "0", "T", "T2", "391", "preempt")
         ])
-        self.fake_reader_special_lines_correct_data = FakeReader([
+        wrong_events_search(fake_reader, self.fake_writer)
+        self.assertEqual(self.fake_writer.result, [('43', 1, 'Wrong order of events. First action is not "activate"')])
+
+    def test_empty_lines_and_comments_with_correct_data(self):
+        fake_reader = FakeReader([
             ("42", "Core1", "0", "T", "T2", "391", "activate"),
             ("#"),
             ("43", "Core1", "0", "T", "T2", "391", "start"),
             (),
             ("44", "Core1", "0", "T", "T2", "391", "terminate")
         ])
-        self.fake_reader_special_lines_incorrect_data = FakeReader([
+        wrong_events_search(fake_reader, self.fake_writer)
+        self.assertFalse(self.fake_writer.result)
+
+    def test_empty_lines_and_comments_with_incorrect_data(self):
+        fake_reader = FakeReader([
             ("#"),
             ("43", "Core1", "0", "T", "T2", "391", "activate"),
             (),
             ("#"),
             ("44", "Core1", "0", "T", "T2", "391", "terminate")
         ])
-
-    def test_simultaneous_start(self):
-        wrong_events_search(self.fake_reader_parallel_start, self.fake_writer)
-        self.assertEqual(self.fake_writer.result, [('45', 4, 'Parallel active tasks')])
-
-    def test_simultaneous_start_resume(self):
-        wrong_events_search(self.fake_reader_parallel_start_resume, self.fake_writer)
-        self.assertEqual(self.fake_writer.result, [('47', 6, 'Parallel active tasks')])
-
-    def test_wrong_order1(self):
-        wrong_events_search(self.fake_reader_wrong_order1, self.fake_writer)
-        self.assertEqual(self.fake_writer.result, [('44', 3, 'Wrong order of events')])
-
-    def test_wrong_order2(self):
-        wrong_events_search(self.fake_reader_wrong_order2, self.fake_writer)
-        self.assertEqual(self.fake_writer.result, [('45', 4, 'Wrong order of events')])
-
-    def test_first_start(self):
-        wrong_events_search(self.fake_reader_first_event_start, self.fake_writer)
-        self.assertEqual(self.fake_writer.result, [('43', 1, 'Wrong order of events. First action is not "activate"')])
-
-    def test_first_preempt(self):
-        wrong_events_search(self.fake_reader_first_event_preempt, self.fake_writer)
-        self.assertEqual(self.fake_writer.result, [('43', 1, 'Wrong order of events. First action is not "activate"')])
-
-    def test_empty_lines_and_comments_with_correct_data(self):
-        res = wrong_events_search(self.fake_reader_special_lines_correct_data, self.fake_writer)
-        self.assertIsNone(res)
-
-    def test_empty_lines_and_comments_with_incorrect_data(self):
-        wrong_events_search(self.fake_reader_special_lines_incorrect_data, self.fake_writer)
+        wrong_events_search(fake_reader, self.fake_writer)
         self.assertEqual(self.fake_writer.result, [('44', 5, 'Wrong order of events')])
 
 
